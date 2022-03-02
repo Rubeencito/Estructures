c56476cfdd2b804ae0b76185e8b0a48fc0ffe1d8
Rubeencito <ruben.huertas@institutvidreres.cat>
2022-03-02 10:52:58 +0100

    EX3

--- estructures.js
+++ estructures.js
@@ -7,7 +7,17 @@ document.write("El primer valor de l'array és ====> " + array[0] + "<br/>")
 document.write("L'últim valor de l'array és ====> " + array[4])
 */
 
+//EX2
+/*
 let array = Array.from({length: 5}, () => Math.floor(Math.random() *10))
     document.write("Array inicial ====> " + array)
 let array2 = [11,33, ...array]
     document.write("<br/>Array afegint els valors de l'anterior ====> " + array2)
+*/
+
+const array1 = [1,2,3,4,5]
+const array2 = [1,2,3,4,5,6,7]
+const arrayFusionat = array1.concat(array2);
+const arrayFinal = new Set (arrayFusionat);
+console.log(arrayFinal)
+
