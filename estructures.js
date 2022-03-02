dcc8eccdd2569fdd0f8b3f81a05f0eef0fa45080
Rubeencito <ruben.huertas@institutvidreres.cat>
2022-03-02 10:47:26 +0100

    EX1

---
+++ estructures.js
@@ -0,0 +1,5 @@
+let array = Array.from({length: 5}, () => Math.floor(Math.random() *10))
+document.write("L'array aleatori és ====> " + array + "<br/>")
+document.write("L'array ordenat és ====> " + array.sort() + "<br/>")
+document.write("El primer valor de l'array és ====> " + array[0] + "<br/>")
+document.write("L'últim valor de l'array és ====> " + array[4])
