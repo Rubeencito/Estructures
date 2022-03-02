00f1df73bd95f948ef679fc287620b990578fbea
Rubeencito <ruben.huertas@institutvidreres.cat>
2022-03-02 10:51:30 +0100

    EX2

--- estructures.js
+++ estructures.js
@@ -1,5 +1,13 @@
+//EX1
+/*
 let array = Array.from({length: 5}, () => Math.floor(Math.random() *10))
 document.write("L'array aleatori és ====> " + array + "<br/>")
 document.write("L'array ordenat és ====> " + array.sort() + "<br/>")
 document.write("El primer valor de l'array és ====> " + array[0] + "<br/>")
 document.write("L'últim valor de l'array és ====> " + array[4])
+*/
+
+let array = Array.from({length: 5}, () => Math.floor(Math.random() *10))
+    document.write("Array inicial ====> " + array)
+let array2 = [11,33, ...array]
+    document.write("<br/>Array afegint els valors de l'anterior ====> " + array2)
